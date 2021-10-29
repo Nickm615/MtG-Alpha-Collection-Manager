90c801946f3afbf35b028b2573426218b9730eef
Nick Margaritondo <86981780+Nickm615@users.noreply.github.com>
2021-10-29 16:56:00 -0500

    Added initial files and created file structure for MVC.

---
+++ server.js
@@ -0,0 +1,32 @@
+//I copied this directly from mini-project unit 14. We can delete what we don't need, add what we do, etc. --Nick
+
+const express = require('express');
+const session = require('express-session');
+const routes = require('./controllers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening at${port}`));
+});
