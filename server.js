56f7b73c23c757d9ad0431859854cec7d8f6337f
Garrison <79775636+segarrison@users.noreply.github.com>
2021-11-01 11:13:07 -0500

    saving for pull

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
 //I copied this directly from mini-project unit 14. We can delete what we don't need, add what we do, etc. --Nick
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const passport = require("passport");
+const bcrypt = require("bcrypt");
+const routes = require("./controllers");
+// const helpers = require('')
 
-const express = require('express');
-const session = require('express-session');
-const routes = require('./controllers');
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
+  secret: "Super secret secret",
+  cookie: { maxAge: 86400000 },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
