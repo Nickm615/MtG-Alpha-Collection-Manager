98f261aa5db7e3599da4ac74b74ff1c7b9645e3d
Garrison <79775636+segarrison@users.noreply.github.com>
2021-11-02 15:37:35 -0500

    code for authcontroller.js

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,5 +1,46 @@
-var exports = (module.exports = {});
-
-exports.signup = function(req, res){
-    res.render('signup');
-}
+module.exports = (app, passport) => {
+    console.log(passport);
+    app.get("/", (req, res) => {
+      res.render("index");
+    });
+  
+    app.get("/signup", (req, res) => {
+      res.render("signup");
+    });
+  
+    app.get("/signin", (req, res) => {
+      res.render("signin");
+    });
+  
+    app.post(
+      "/signup",
+      passport.authenticate("local-signup", {
+        successRedirect: "/collection",
+        failureRedirect: "/index",
+      })
+    );
+  
+    app.get("/collection", isLoggedIn, (req, res) => {
+      res.render("collection");
+    });
+  
+    app.get("/logout", (req, res) => {
+      req.session.destroy((err) => {
+        res.redirect("/");
+      });
+    });
+  
+    app.post(
+      "/signin",
+      passport.authenticate("local-signin", {
+        successRedirect: "/collection",
+        failureRedirect: "/",
+      })
+    );
+  
+    function isLoggedIn(req, res, next) {
+      if (req.isAuthenticated()) return next();
+  
+      res.redirect("/signin");
+    }
+  };
