4c08d8c0bb9970ae273832f6489ad5ba7034a8d1
nskidmore7 <njskid11@gmail.com>
2021-11-02 11:56:49 -0500

    Change in seeding

--- seeds/cardData.js
+++ seeds/cardData.js
@@ -1,10 +1,10 @@
 const  Card  = require('../models/Card');
 const mtg = require('mtgsdk');
-const e = require('express');
+// const e = require('express');
 const seedCards = (data) => Card.bulkCreate(data);
 mtg.card.where({setName: 'Limited Edition Alpha', pageSize:10})
 .then(cards => {
-    // console.log(cards)
+     //console.log(cards)
   
     const cardData =  cards.map(e =>
        {
@@ -22,7 +22,7 @@ mtg.card.where({setName: 'Limited Edition Alpha', pageSize:10})
    
     
 })
-// module.exports = seedCards;
+ module.exports = seedCards;
 // console.log(cardData)
 
 
