c92fbdf9023518d2f24938cd8b28d820ccc9d87e
nskidmore7 <njskid11@gmail.com>
2021-11-02 11:14:56 -0500

    Get Request

--- server.js
+++ server.js
@@ -38,3 +38,48 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening at${PORT}`));
 });
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/views/collection.handlebars'))
+);
+
+// GET request for cards
+app.get('/api/Card', async (req, res) => {
+  console.info(`GET /api/Card`);
+  res.status(200).json(cards);
+  try{
+    const dbCardData = await Card.findAll({
+        
+    });
+    const cards = dbCardData.map((e) => 
+        e.get({plain: true})
+        
+    );
+    res.render('card-list', {
+        cards,
+       // loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  };
+});
+
+// GET a single card
+app.get('/api/Card/:Card_name', (req, res) => {
+  if (req.params.Card_name) {
+    console.info(`${req.method} request received to get a Card`);
+    const cardName = req.params.card_name;
+    for (let i = 0; i < cards.length; i++) {
+      const currentCard = cards[i];
+      if (currentCard.Card_name === cardName) {
+        res.json(currentCard);
+        return;
+      }
+    }
+    res.status(404).send('Card not found');
+  } else {
+    res.status(400).send('Card name not provided');
+  }
+});
+
