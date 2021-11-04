2ed741a4b26225c8ff08420e67b3eee6195dc260
Garrison <79775636+segarrison@users.noreply.github.com>
2021-11-04 12:50:32 -0500

    trying to fix heroku issue

--- models/Collection.js
+++ models/Collection.js
@@ -1,51 +1,47 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
-const Card = require('./Card');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+const Card = require("./Card");
 
-class Collection extends Model{}
+class Collection extends Model {}
 
 Collection.init(
-    {
-        card_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'card',
-                key: 'id',
-                unique: false
-                
-            }
-
-        },
-        quantity: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            defaultValue: 1
-
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'user',
-                key: 'id',
-                unique: false
-            }
-        },
-        imageUrl: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            references: {
-                model: 'card',
-                key: 'imageUrl'
-            }
-        }
-        
+  {
+    card_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "card",
+        key: "id",
+        unique: false,
+      },
     },
-    {
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+        unique: false,
+      },
+    },
+    imageUrl: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: "card",
+        key: "imageUrl",
+      },
+    },
+  },
+  {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     // underscored: true,
-    modelName: 'collection'
-    }
-)
+    modelName: "collection",
+  }
+);
 module.exports = Collection;
